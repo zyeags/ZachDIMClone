b2e9194c5d7437e6e275ca7d5ca9e8bff01f6566
zyeags <104327074+zyeags@users.noreply.github.com>
2022-04-24 16:53:31 -0500

    First Change Test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Inventory from "./containers/Inventory";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Hello Destiny Item Manager</h1>
       <h2>Start editing to see some magic happen!</h2>
 
       <Inventory />
