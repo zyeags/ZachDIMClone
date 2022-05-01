beb76781ed340ab80ae21ecbaff23fca29ba3e69
Keith Holliday <krh121791@yahoo.com>
2022-05-01 17:45:29 -0600

    initial commit

--- src/containers/Inventory.js
+++ src/containers/Inventory.js
@@ -1,17 +1,40 @@
+import { useState } from "react";
 import ItemSection from "../components/ItemSection";
 
+const defaultEquipped = {
+  warlock: "test",
+  titan: "",
+  hunter: ""
+};
+
 export default function Inventory() {
+  const [equipped, setEquipped] = useState(defaultEquipped);
+
+  function equipWeapon(weapon, characterClass) {
+    const copyEquipped = { ...equipped };
+    copyEquipped[characterClass] = weapon;
+    setEquipped(copyEquipped);
+  }
+
   return (
     <div>
-      <h2>Inventory</h2>
+      <h2>Inventory2</h2>
+
+      <div>
+        warlock: {equipped.warlock}
+        titan: {equipped.titan}
+        hunter: {equipped.hunter}
+      </div>
+
       <ItemSection
         name="Postmaster"
         weapons={["Divinity,", " Gjally,"]}
         armor={["Lost Pacific Helm"]}
+        general={["Exotic Ghost Shell"]}
         currency={["Strange Coins,", " Treasure Keys,", " Enhancement Cores"]}
       />
-      <ItemSection name="Weapons" />
-      <ItemSection name="Armor" />
+      <ItemSection name="Weapons" equipWeapon={equipWeapon} />
+      <ItemSection name="Armor" equipWeapon={equipWeapon} />
       <ItemSection name="General" />
       <ItemSection name="Inventory" />
     </div>
