cf2b92a3c9661ed55adad82c85d5112a615b6b0b
zyeags <104327074+zyeags@users.noreply.github.com>
2022-05-03 07:20:39 -0500

    [2] Basic Character Class Card Created box and background image for basic character class card.

--- src/containers/Characters.js
+++ src/containers/Characters.js
@@ -2,38 +2,43 @@ import CharacterSection from "../components/CharacterSection";
 
 export default function Characters() {
   return (
-    <div>
-      <h2>Characters</h2>
-      <CharacterSection
-        name="Hunter"
-        maximumPower={1560}
-        mobility={39}
-        resilience={64}
-        recovery={73}
-        discipline={34}
-        intellect={43}
-        strength={105}
-      />
-      <CharacterSection
-        name="Warlock"
-        maximumPower={1449}
-        mobility={40}
-        resilience={46}
-        recovery={42}
-        discipline={62}
-        intellect={47}
-        strength={32}
-      />
-      <CharacterSection
-        name="Titan"
-        maximumPower={1449}
-        mobility={39}
-        resilience={36}
-        recovery={32}
-        discipline={34}
-        intellect={38}
-        strength={38}
-      />
-    </div>
+    <body>
+      <div>
+        <h2>Characters</h2>
+        <div class="Character"></div>
+        <div class="Character"></div>
+        <div class="Character"></div>
+        <CharacterSection
+          name="Hunter"
+          maximumPower={1560}
+          mobility={39}
+          resilience={64}
+          recovery={73}
+          discipline={34}
+          intellect={43}
+          strength={105}
+        />
+        <CharacterSection
+          name="Warlock"
+          maximumPower={1449}
+          mobility={40}
+          resilience={46}
+          recovery={42}
+          discipline={62}
+          intellect={47}
+          strength={32}
+        />
+        <CharacterSection
+          name="Titan"
+          maximumPower={1449}
+          mobility={39}
+          resilience={36}
+          recovery={32}
+          discipline={34}
+          intellect={38}
+          strength={38}
+        />
+      </div>
+    </body>
   );
 }
