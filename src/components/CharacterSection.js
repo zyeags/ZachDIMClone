04b2252dde8255d2bda16e05acd60df429151c1e
zyeags <104327074+zyeags@users.noreply.github.com>
2022-05-11 09:04:57 -0500

    Stat Images

--- src/components/CharacterSection.js
+++ src/components/CharacterSection.js
@@ -3,13 +3,90 @@ export default function CharacterSection(props) {
     <div>
       <strong>{props.name}</strong>
       <ul>
-        <li>Maximum Power: {props.maximumPower}</li>
-        <li>Mobility: {props.mobility}</li>
-        <li>Resilience: {props.resilience}</li>
-        <li>Recovery: {props.recovery}</li>
-        <li>Discipline: {props.discipline}</li>
-        <li>Intellect: {props.intellect}</li>
-        <li>Strength: {props.strength}</li>
+        <li>
+          {
+            <img
+              value="maxPower"
+              width="20px"
+              height="20x"
+              src="https://www.bungie.net/common/destiny2_content/icons/7c30e0e489e51a3920b4446684fbcdb1.png"
+              alt="maxPower"
+            />
+          }
+          : {props.maximumPower}
+        </li>
+        <li>
+          {
+            <img
+              value="mobility"
+              width="20px"
+              height="20x"
+              src="https://www.bungie.net/common/destiny2_content/icons/e26e0e93a9daf4fdd21bf64eb9246340.png"
+              alt="mobility"
+            />
+          }
+          : {props.mobility}
+        </li>
+        <li>
+          {
+            <img
+              value="resilience"
+              width="20px"
+              height="20x"
+              src="https://www.bungie.net/common/destiny2_content/icons/202ecc1c6febeb6b97dafc856e863140.png"
+              alt="resilience"
+            />
+          }
+          : {props.resilience}
+        </li>
+        <li>
+          {
+            <img
+              value="recovery"
+              width="20px"
+              height="20x"
+              src="https://www.bungie.net/common/destiny2_content/icons/128eee4ee7fc127851ab32eac6ca91cf.png"
+              alt="recovery"
+            />
+          }
+          : {props.recovery}
+        </li>
+        <li>
+          {
+            <img
+              value="discipline"
+              width="20px"
+              height="20x"
+              src="https://www.bungie.net/common/destiny2_content/icons/ca62128071dc254fe75891211b98b237.png"
+              alt="discipline"
+            />
+          }
+          : {props.discipline}
+        </li>
+        <li>
+          {
+            <img
+              value="intellect"
+              width="20px"
+              height="20x"
+              src="https://www.bungie.net/common/destiny2_content/icons/59732534ce7060dba681d1ba84c055a6.png"
+              alt="intellect"
+            />
+          }
+          : {props.intellect}
+        </li>
+        <li>
+          {
+            <img
+              value="strength"
+              width="20px"
+              height="20x"
+              src="https://www.bungie.net/common/destiny2_content/icons/c7eefc8abbaa586eeab79e962a79d6ad.png"
+              alt="strength"
+            />
+          }
+          : {props.strength}
+        </li>
       </ul>
     </div>
   );
