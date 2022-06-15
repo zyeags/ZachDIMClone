6d029bfbba2ebfedf3818ac0390ce76f971ef510
zyeags <104327074+zyeags@users.noreply.github.com>
2022-06-15 08:58:43 -0500

    [6] Ranks to Progress Tab

--- src/components/Ranks.js
+++ src/components/Ranks.js
@@ -2,7 +2,21 @@ export default function Ranks(props) {
   return (
     <div>
       <div className="RanksSection">
-        <div className="CrucibleRank"></div>
+        <div className="CrucibleRank">
+          {
+            <img
+              value="crucible"
+              width="100px"
+              height="100px"
+              src="https://www.bungie.net/common/destiny2_content/icons/6db02eca22b460859566ab813bd3a24b.png"
+              alt="cruciblerank"
+            />
+          }
+          <div className="CrucibleRankInfo">
+            <div className="CrucibleRankLevel"> Crucible Rank 14</div>
+            <div className="CrucibleRankTitle"> Mythic II</div>
+          </div>
+        </div>
       </div>
     </div>
   );
