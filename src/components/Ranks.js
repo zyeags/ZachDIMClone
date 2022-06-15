c710ccc0cc5587f026c3ee15b820869b30459473
zyeags <104327074+zyeags@users.noreply.github.com>
2022-06-15 17:12:24 -0500

    [6] ranks to progress tab

--- src/components/Ranks.js
+++ src/components/Ranks.js
@@ -5,16 +5,59 @@ export default function Ranks(props) {
         <div className="CrucibleRank">
           {
             <img
-              value="crucible"
+              value="crucibleIcon"
               width="100px"
               height="100px"
               src="https://www.bungie.net/common/destiny2_content/icons/6db02eca22b460859566ab813bd3a24b.png"
-              alt="cruciblerank"
+              alt="crucibleIcon"
             />
           }
           <div className="CrucibleRankInfo">
-            <div className="CrucibleRankLevel"> Crucible Rank 14</div>
-            <div className="CrucibleRankTitle"> Mythic II</div>
+            <div className="rankLevel"> Crucible Rank 14</div>
+            <div className="rankTitle"> Mythic II</div>
+            <div className="rankNumber">
+              {
+                <img
+                  value="cruciblerank"
+                  width="10px"
+                  height="10px"
+                  src="https://www.bungie.net/img/theme/destiny/icons/icon_pvprank_orange.png"
+                  alt="crucibleRankIcon"
+                />
+              }
+              {"7119 (669/1075)"}
+            </div>
+            <div className="rankCheckboxes">checkboxes</div>
+            <div className="percentToComplete">{"71% to reset"}</div>
+          </div>
+        </div>
+        <div className="gloryRank">
+          {
+            <img
+              value="gloryIcon"
+              width="100px"
+              height="100px"
+              src="https://www.bungie.net/common/destiny2_content/icons/34586ea2c4de2ce5d0016b3d01cec934.png"
+              alt="crucibleIcon"
+            />
+          }
+          <div className="CrucibleRankInfo">
+            <div className="rankLevel"> Glory Rank 1</div>
+            <div className="rankTitle"> Guardian I</div>
+            <div className="rankNumber">
+              {
+                <img
+                  value="cruciblerank"
+                  width="10px"
+                  height="10px"
+                  src="https://www.bungie.net/img/theme/destiny/icons/icon_pvprank_orange.png"
+                  alt="crucibleRankIcon"
+                />
+              }
+              {"0 (0/40)"}
+            </div>
+            <div className="rankCheckboxes">checkboxes</div>
+            <div className="percentToComplete">{"0% to reset"}</div>
           </div>
         </div>
       </div>
